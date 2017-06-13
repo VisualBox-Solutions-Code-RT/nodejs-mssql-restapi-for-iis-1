836eda1b115abb0649139e14cd6dd55dc018e6b7
jermiedomingo <8jermiedomingo@gmail.com>
2017-06-13 16:00:48 +0400

    cleanup before initial build to preprod

--- app/routes/resources/releases.js
+++ app/routes/resources/releases.js
@@ -35,73 +35,75 @@ module.exports = (apiRouter) => {
 
   // FOR RELEASES
   // routing for your_url/api/releases request
-  apiRouter.route('/releases/:release_id').get((req, res) => {
+  apiRouter.route('/releases/:release_id')
 
-    let releaseId = req.params.release_id;
+    .get((req, res) => {
 
-    // if url has a query parameter
-    // e.g. api/releases/1?complete=true
-    // append to the mssql query
-    // This is to aggregate all Release values in one JSON response
-    const isQueryForComplete = req.query.complete;
+      let releaseId = req.params.release_id;
 
-    if (isQueryForComplete === 'true') {
+      // if url has a query parameter
+      // e.g. api/releases/1?complete=true
+      // append to the mssql query
+      // This is to aggregate all Release values in one JSON response
+      const isQueryForComplete = req.query.complete;
 
-      async.series([
-        (callback) => {
-          request.get(`${AppUrl}/api/releases/${releaseId}`, callback)
-        },
-        (callback) => {
-          request.get(`${AppUrl}/api/components?releaseID=${releaseId}`, callback)
-        },
-        (callback) => {
-          request.get(`${AppUrl}/api/milestones?releaseID=${releaseId}`, callback)
-        }
-      ],
+      if (isQueryForComplete === 'true') {
 
-      //callback
-      (err, results) => {
-        if (err) {
-          jsonHelper(res).error();
-          return;
-        }
+        async.series([
+          (callback) => {
+            request.get(`${AppUrl}/api/releases/${releaseId}`, callback)
+          },
+          (callback) => {
+            request.get(`${AppUrl}/api/components?releaseID=${releaseId}`, callback)
+          },
+          (callback) => {
+            request.get(`${AppUrl}/api/milestones?releaseID=${releaseId}`, callback)
+          }
+        ],
 
-        let aggregatedData = {
-          releaseInfo: JSON.parse(results[0][1]),
-          components: JSON.parse(results[1][1]),
-          milestones: JSON.parse(results[2][1])
-        }
+        //callback
+        (err, results) => {
+          if (err) {
+            jsonHelper(res).error();
+            return;
+          }
 
-        //return json as response
-        jsonHelper(res).callback('get', aggregatedData);
-      })
-    } else {
-      let query = `SELECT * FROM brm.dbo.Releases WHERE ID = ${releaseId}`;
+          let aggregatedData = {
+            releaseInfo: JSON.parse(results[0][1]),
+            components: JSON.parse(results[1][1]),
+            milestones: JSON.parse(results[2][1])
+          }
 
-      //connect to your database & return json response
-      sqlHelper.queryDB(query, jsonHelper(res).callback, jsonHelper(res).error, 'read');
-    }
+          //return json as response
+          jsonHelper(res).callback('get', aggregatedData);
+        })
+      } else {
+        let query = `SELECT * FROM brm.dbo.Releases WHERE ID = ${releaseId}`;
 
-  })
+        //connect to your database & return json response
+        sqlHelper.queryDB(query, jsonHelper(res).callback, jsonHelper(res).error, 'read');
+      }
+
+    })
 
   // PUT ENDPOINT
   // FOR UPDATING RELEASE INFO
     .put((req, res) => {
 
-    let releaseId = req.params.release_id;
+      let releaseId = req.params.release_id;
 
-    const {title, RFCNumber, owner, goLiveDate, status} = req.body;
+      const {title, RFCNumber, owner, goLiveDate, status} = req.body;
 
-    let query = `UPDATE Releases SET
-          Title =  '${title}',
-          RFCNumber = '${RFCNumber}',
-          Owner = '${owner}',
-          GoLiveDate = '${goLiveDate}',
-          Status = '${status}'
-          WHERE ID = ${releaseId}`;
+      let query = `UPDATE Releases SET
+            Title =  '${title}',
+            RFCNumber = '${RFCNumber}',
+            Owner = '${owner}',
+            GoLiveDate = '${goLiveDate}',
+            Status = '${status}'
+            WHERE ID = ${releaseId}`;
 
-    //connect to your database & return json response
-    sqlHelper.queryDB(query, jsonHelper(res).callback, jsonHelper(res).error, 'update');
+      //connect to your database & return json response
+      sqlHelper.queryDB(query, jsonHelper(res).callback, jsonHelper(res).error, 'update');
 
-  });
+    });
 }
