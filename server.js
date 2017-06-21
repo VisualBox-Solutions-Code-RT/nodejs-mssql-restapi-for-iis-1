d2f392e620b9207b292778d7e36bd42c86f5444c
jermiedomingo <8jermiedomingo@gmail.com>
2017-06-21 12:29:21 +0400

    removing cors module dependency, enabling CORS manually

--- server.js
+++ server.js
@@ -8,23 +8,21 @@ const express = require('express'); //call express
 const app = express(); //define our app using express
 const bodyParser = require('body-parser'); //get body-parser will let us pull POST content from our HTTP request
 const morgan = require('morgan'); //used to log requests on the console
-var cors = require('cors'); //for allowing CORS
 const AppConfig = require('./app/config').AppConfig;
 
 //APP CONFIGURATION ---------------
 //use body-parser so we can grab information from POST REQUESTS
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//enable cors
-app.use(cors());
-app.options('*', cors()); 
 
 // configure our app to handle CORS requests
 app.use(function(req, res, next){
 
 	res.setHeader('Access-Control-Allow-Origin','*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Credentials','true');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+
 	next();
 
 });
